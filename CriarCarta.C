1fed9f7de59b419d04e53758dc7b324274fc9a84
NaanArmaX <tainan56.tss@gmail.com>
2025-09-02 19:28:14 -0300

    Desafio intermediario

--- CriarCarta.C
+++ CriarCarta.C
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
+
+char estado[3];
+char codcarta[10]; 
+char nome_cidade[50];
+int populacao;
+float area;
+float pib;
+float Densidade_Populacao;
+float PIB_per_capita;
+int pontos_t;
+int c;
+
+void DenPopu(){
+
+Densidade_Populacao = (float) populacao / area;
+
+};
+
+void PIBCapita(){
+
+PIB_per_capita = (float) pib / populacao;
+
+};
+
+
 int main() {
-    char estado[3];
-    char codcarta[10]; 
-    char nome_cidade[50];
-    int populacao;
-    float area;
-    float pib;
-    int pontos_t;
-    int c;
+ 
 
     printf("Insira as cartas de super triunfo\n\n");
     printf("Primeira Carta\n\n");
@@ -40,9 +58,20 @@ int main() {
     printf("Insira quantidade de pontos turisticos: ");
     scanf("%d", &pontos_t);
 
-    printf("\nCarta Registrada:\n");
-    printf("Estado: %s\nCódigo: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nPontos Turísticos: %d\n",
-           estado, codcarta, nome_cidade, populacao, area, pib, pontos_t);
+    DenPopu();
 
+    PIBCapita();
+
+    printf("\nCarta Registrada:\n");
+    printf("\nCarta Registrada:\n");
+    printf("Estado: %s\n", estado);
+    printf("Código: %s\n", codcarta);
+    printf("Cidade: %s\n", nome_cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Pontos Turísticos: %d\n", pontos_t);
+    printf("Densidade Demográfica: %.2f hab/km²\n", Densidade_Populacao);
+    printf("PIB Per Capita: %.2f reais\n", PIB_per_capita);
     return 0;
 }
